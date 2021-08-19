e0e446b4504499adf3883796230af8b0132f916e
lnekrasov <lnekrasov@beget.ru>
2021-08-19 20:01:00 +0300

    first files

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+use CodingLiki\PhpMvc\App\App;
+
+require_once "./vendor/CodingLiki/OoAutoloader/Autoloader.php";
+require_once "./vendor/CodingLiki/View/include/helpers.php";
+
+$app = App::getInstance();
+
+$app->start("app_middlewares", [ "routes.routes" ]);
